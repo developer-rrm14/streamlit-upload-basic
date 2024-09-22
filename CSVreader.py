e54ad8de414b0d45d45f782076b771f8773fcf0a
Roberto Melo <robertoreboucasdemelo@gmail.com>
2024-09-22 00:35:43 -0300

    Add Project

---
+++ CSVreader.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(page_title="CSV File Reader", layout="wide")
+st.header("Single File Upload")
+upload_file = st.file_uploader("Uploader your CSV File")
+df = pd.read_csv(upload_file)
+st.dataframe(df, width=1000, height=1200)
